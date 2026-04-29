9a8fe5594b02ae502f3dd3d35fba1cbe98a5a757
Egi Mancellari <mancellariegi@gmail.com>
2026-04-29 00:23:08 -0500

    Completed length assignment

--- the-string-dot-length-property.js
+++ the-string-dot-length-property.js
@@ -25,6 +25,10 @@ let lengthTwo;   // Length of stringTwo
 let lengthThree; // Length of stringThree
 
 // Your code here
+lengthOne = stringOne.length;
+lengthTwo = stringTwo.length;
+lengthThree = stringThree.length;
+
 
 
 
